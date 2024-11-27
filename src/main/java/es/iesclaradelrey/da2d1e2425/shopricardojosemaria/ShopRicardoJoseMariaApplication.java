ea5143819324ddf9276e18f0f0eeb11b91e5e7d7
jomconcal <jomconcal@gmail.com>
2024-11-27 19:31:25 +0100

    inicio Proyecto

---
+++ src/main/java/es/iesclaradelrey/da2d1e2425/shopricardojosemaria/ShopRicardoJoseMariaApplication.java
@@ -0,0 +1,13 @@
+package es.iesclaradelrey.da2d1e2425.shopricardojosemaria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopRicardoJoseMariaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopRicardoJoseMariaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/iesclaradelrey/da2d1e2425/shopricardojosemaria/ShopRicardoJoseMariaApplicationTests.java
@@ -0,0 +1,13 @@
+package es.iesclaradelrey.da2d1e2425.shopricardojosemaria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopRicardoJoseMariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
