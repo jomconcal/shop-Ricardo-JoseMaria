9f704f81350beeb1614f65dd129cc3f3a1a9eb90
JoseMariaConcha <jomconcal@gmail.com>
2024-12-11 00:25:10 +0100

    add products

--- src/main/java/es/iesclaradelrey/da2d1e2425/shopricardojosemaria/commandLineRunners/CategoryCommandLineRunner.java
+++ src/main/java/es/iesclaradelrey/da2d1e2425/shopricardojosemaria/commandLineRunners/CategoryCommandLineRunner.java
@@ -18,17 +18,87 @@ public class CategoryCommandLineRunner implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
 
-        long id= 1L;
-        Category coffee = new Category(id++, "Coffee", "The best coffee quality", "/img/menu-item-1.jpg");
-        Category tea = new Category(id++, "Tea", "Our teas has the best flavour in the market", "/img/menu-item-2.jpg");
+        long id = 1L;
 
-        Product machiato= new Product(id++,coffee,"Macchiato","Soft coffee",5.95,"/img/menu-item-3.jpg");
-        Product roibos= new Product(id++,tea,"Roibos","Nice infusion",7.00,"/img/menu-item-4.jpg");
+// Categorías
+        Category coffee = new Category(id++, "Coffees", "Enjoy the finest coffee blends", "/img/coffee.jpg");
+        Category tea = new Category(id++, "Teas", "Discover the magic of our tea selections", "/img/tea.jpg");
+        Category cocktail = new Category(id++, "Cocktails", "Crafted cocktails for every occasion", "/img/cocktail.jpg");
+        Category pintxo = new Category(id++, "Pintxos", "Basque-inspired gourmet bites", "/img/pintxo.jpg");
+        Category dessert = new Category(id++, "Desserts", "Sweet delights to end your meal", "/img/dessert.jpg");
 
+// Productos de Coffees
+        Product espresso = new Product(id++, coffee, "Espresso", "Classic Italian coffee", 4.50, "/img/espresso.jpg");
+        Product cappuccino = new Product(id++, coffee, "Cappuccino", "Perfect blend of coffee and milk foam", 5.50, "/img/cappuccino.jpg");
+        Product latte = new Product(id++, coffee, "Latte", "Smooth coffee with steamed milk", 5.00, "/img/latte.jpg");
+        Product americano = new Product(id++, coffee, "Americano", "Simple and bold black coffee", 4.00, "/img/americano.jpg");
+        Product mocha = new Product(id++, coffee, "Mocha", "Coffee with a chocolate twist", 5.75, "/img/mocha.jpg");
+
+// Productos de Teas
+        Product greenTea = new Product(id++, tea, "Green Tea", "Refreshing and healthy", 3.50, "/img/green-tea.jpg");
+        Product blackTea = new Product(id++, tea, "Black Tea", "Strong and full-bodied", 3.75, "/img/black-tea.jpg");
+        Product chamomile = new Product(id++, tea, "Chamomile", "Calming herbal tea", 4.00, "/img/chamomile.jpg");
+        Product oolong = new Product(id++, tea, "Oolong", "A perfect balance of flavor", 4.25, "/img/oolong.jpg");
+        Product mintTea = new Product(id++, tea, "Mint Tea", "Refreshing mint flavor", 3.50, "/img/mint-tea.jpg");
+
+// Productos de Cocktails
+        Product mojito = new Product(id++, cocktail, "Mojito", "Classic Cuban cocktail", 8.00, "/img/mojito.jpg");
+        Product margarita = new Product(id++, cocktail, "Margarita", "A citrusy favorite", 9.00, "/img/margarita.jpg");
+        Product negroni = new Product(id++, cocktail, "Negroni", "Bold and sophisticated", 10.00, "/img/negroni.jpg");
+        Product piñaColada = new Product(id++, cocktail, "Piña Colada", "Tropical pineapple delight", 8.50, "/img/pina-colada.jpg");
+        Product daiquiri = new Product(id++, cocktail, "Daiquiri", "Refreshing lime cocktail", 9.50, "/img/daiquiri.jpg");
+
+// Productos de Pintxos
+        Product jamonPintxo = new Product(id++, pintxo, "Jamon Pintxo", "Ham on crusty bread", 3.50, "/img/jamon-pintxo.jpg");
+        Product tortillaPintxo = new Product(id++, pintxo, "Tortilla Pintxo", "Spanish omelette bite", 3.75, "/img/tortilla-pintxo.jpg");
+        Product shrimpPintxo = new Product(id++, pintxo, "Shrimp Pintxo", "Shrimp with garlic", 4.00, "/img/shrimp-pintxo.jpg");
+        Product chorizoPintxo = new Product(id++, pintxo, "Chorizo Pintxo", "Spicy sausage and pepper", 4.25, "/img/chorizo-pintxo.jpg");
+        Product cheesePintxo = new Product(id++, pintxo, "Cheese Pintxo", "Artisan cheese selection", 4.50, "/img/cheese-pintxo.jpg");
+
+// Productos de Desserts
+        Product brownie = new Product(id++, dessert, "Brownie", "Rich chocolate dessert", 4.50, "/img/brownie.jpg");
+        Product cheesecake = new Product(id++, dessert, "Cheesecake", "Creamy and indulgent", 5.00, "/img/cheesecake.jpg");
+        Product tiramisu = new Product(id++, dessert, "Tiramisu", "Classic Italian dessert", 5.50, "/img/tiramisu.jpg");
+        Product pannaCotta = new Product(id++, dessert, "Panna Cotta", "Silky and smooth", 5.25, "/img/panna-cotta.jpg");
+        Product iceCream = new Product(id++, dessert, "Ice Cream", "Assorted gourmet flavors", 4.00, "/img/ice-cream.jpg");
+
+// Guardar categorías
         categoryService.save(coffee);
         categoryService.save(tea);
+        categoryService.save(cocktail);
+        categoryService.save(pintxo);
+        categoryService.save(dessert);
 
-        productService.save(machiato);
-        productService.save(roibos);
+// Guardar productos
+        productService.save(espresso);
+        productService.save(cappuccino);
+        productService.save(latte);
+        productService.save(americano);
+        productService.save(mocha);
+
+        productService.save(greenTea);
+        productService.save(blackTea);
+        productService.save(chamomile);
+        productService.save(oolong);
+        productService.save(mintTea);
+
+        productService.save(mojito);
+        productService.save(margarita);
+        productService.save(negroni);
+        productService.save(piñaColada);
+        productService.save(daiquiri);
+
+        productService.save(jamonPintxo);
+        productService.save(tortillaPintxo);
+        productService.save(shrimpPintxo);
+        productService.save(chorizoPintxo);
+        productService.save(cheesePintxo);
+
+        productService.save(brownie);
+        productService.save(cheesecake);
+        productService.save(tiramisu);
+        productService.save(pannaCotta);
+        productService.save(iceCream);
+
     }
 }
