221ddf88b6a59907a0206453fc7d4223dc011140
jomconcal <jomconcal@gmail.com>
2025-04-22 20:46:09 +0200

    quitado csrf de web temporalmente

--- src/main/java/es/iesclaradelrey/da2d1e2425/shopricardojosemaria/config/WebSecurityConfig.java
+++ src/main/java/es/iesclaradelrey/da2d1e2425/shopricardojosemaria/config/WebSecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 /**
@@ -44,7 +45,8 @@ public class WebSecurityConfig {
     @Bean
     public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
         // De momento, permitir el acceso a cualquier parte de la aplicación.
-        http.authorizeHttpRequests(auth -> auth
+        http.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
                 .anyRequest().permitAll());
         return http.build();
     }
