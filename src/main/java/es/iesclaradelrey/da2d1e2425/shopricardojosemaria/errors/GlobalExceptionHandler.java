66d5c4206f4f784953e2a1a7da46dfc2172c3d27
jomconcal <jomconcal@gmail.com>
2025-05-14 19:32:54 +0200

    exceptions handled

--- src/main/java/es/iesclaradelrey/da2d1e2425/shopricardojosemaria/errors/GlobalExceptionHandler.java
+++ src/main/java/es/iesclaradelrey/da2d1e2425/shopricardojosemaria/errors/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package es.iesclaradelrey.da2d1e2425.shopricardojosemaria.errors;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -10,22 +11,32 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(ProductNotFoundException.class)
-    public ResponseEntity<String> productNotFound(ProductNotFoundException e) {
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+    public ResponseEntity<ProblemDetail> productNotFound(ProductNotFoundException e) {
+        ProblemDetail problem= ProblemDetail.forStatusAndDetail(HttpStatus.UNAUTHORIZED,e.getMessage());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(problem);
     }
 
     @ExceptionHandler(InsufficientStock.class)
-    public ResponseEntity<String> insufficientStock(InsufficientStock e) {
-        return ResponseEntity.status(HttpStatus.CONFLICT).body(e.getMessage());
+    public ResponseEntity<ProblemDetail> insufficientStock(InsufficientStock e) {
+        ProblemDetail problem= ProblemDetail.forStatusAndDetail(HttpStatus.CONFLICT,e.getMessage());
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(problem);
     }
 
     @ExceptionHandler(IllegalArgumentException.class)
-    public ResponseEntity<String> illegalArgument(IllegalArgumentException e) {
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+    public ResponseEntity<ProblemDetail> illegalArgument(IllegalArgumentException e) {
+        ProblemDetail problem= ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST,e.getMessage());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(problem);
     }
 
     @ExceptionHandler(WrongCredentialsException.class)
-    public ResponseEntity<String> wrongCredentials(WrongCredentialsException e) {
-        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(e.getMessage());
+    public ResponseEntity<ProblemDetail> wrongCredentials(WrongCredentialsException e) {
+        ProblemDetail problem= ProblemDetail.forStatusAndDetail(HttpStatus.UNAUTHORIZED,e.getMessage());
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(problem);
+    }
+
+    @ExceptionHandler(UserNameAlreadyExistsException.class)
+    public ResponseEntity<ProblemDetail> userNameAlreadyExists(UserNameAlreadyExistsException e) {
+        ProblemDetail problem= ProblemDetail.forStatusAndDetail(HttpStatus.CONFLICT,e.getMessage());
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(problem);
     }
 }
